782364e436119e64a2c49b375bad4c7cefead840
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2023-08-28 20:40:28 +0700

    Nambahin Icon Apps sama Homepage sedikit

--- app/src/main/java/com/syafi/ecoquest/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/syafi/ecoquest/presentation/home/HomeScreen.kt
@@ -1,9 +1,62 @@
 package com.syafi.ecoquest.presentation.home
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.syafi.ecoquest.R
+import com.syafi.ecoquest.ui.theme.green
 
 @Composable
 fun HomeScreen() {
-    Text(text = "Home")
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(14.dp)
+        ) {
+            Text(
+                text = "Selamat Datang,",
+                style = MaterialTheme.typography.h5,
+            )
+            Text(
+                text = "Bambang",
+                style = MaterialTheme.typography.h5,
+                color = MaterialTheme.colors.green
+            )
+
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(CircleShape)
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.profile_picture),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Bebas() {
+    HomeScreen()
 }
