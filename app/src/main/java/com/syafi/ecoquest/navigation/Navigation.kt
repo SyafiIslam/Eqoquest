dafe4ec3b956d493d4ab3ff78bbd266f4c3ad6c5
SyafiIslam <muhammadsyafiislam@gmail.com>
2023-08-28 11:10:17 +0700

    add nav

--- app/src/main/java/com/syafi/ecoquest/navigation/Navigation.kt
+++ app/src/main/java/com/syafi/ecoquest/navigation/Navigation.kt
@@ -1,2 +1,12 @@
 package com.syafi.ecoquest.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = "Login") {
+
+    }
+}
