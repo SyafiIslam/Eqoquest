bb8a5ca65cd7340783e8df56c3cc16404cf3088b
Fajri009 <102470811+Fajri009@users.noreply.github.com>
2023-08-30 21:48:42 +0700

    Add Challenge Page

--- app/src/main/java/com/syafi/ecoquest/navigation/Navigation.kt
+++ app/src/main/java/com/syafi/ecoquest/navigation/Navigation.kt
@@ -18,7 +18,7 @@ import com.syafi.ecoquest.util.Routes
 
 @Composable
 fun Navigation(navController: NavHostController, afterSplashDestination: String) {
-    NavHost(navController = navController, startDestination = Routes.CHALLENGE) {
+    NavHost(navController = navController, startDestination = Routes.SPLASH) {
         composable(Routes.SPLASH) {
             SplashScreen(navController = navController, afterSplashDestination)
         }
