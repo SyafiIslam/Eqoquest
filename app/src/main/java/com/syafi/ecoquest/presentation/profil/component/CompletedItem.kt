4baeac663781944dac0f84060121baa1aece3599
SyafiIslam <muhammadsyafiislam@gmail.com>
2023-08-31 20:57:45 +0700

    add profile screen

---
+++ app/src/main/java/com/syafi/ecoquest/presentation/profil/component/CompletedItem.kt
@@ -0,0 +1,40 @@
+package com.syafi.ecoquest.presentation.profil.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import coil.size.Size
+import com.syafi.ecoquest.R
+import com.syafi.ecoquest.model.Completed
+import com.syafi.ecoquest.ui.theme.green
+
+@Composable
+fun Completed(completedData: Completed) {
+    Card(
+        elevation = 0.dp,
+        backgroundColor = MaterialTheme.colors.green,
+        modifier = Modifier.padding(bottom = 15.dp),
+    ) {
+        Row(
+            Modifier
+                .fillMaxWidth()
+                .padding(20.dp)
+        ) {
+            Row(Modifier.weight(1f)) {
+                AsyncImage(
+                    model = R.drawable.check_putih,
+                    contentDescription = "",
+                    modifier = Modifier.size(24.dp)
+                )
+                Spacer(modifier = Modifier.width(10.dp))
+                Text(text = completedData.title, style = MaterialTheme.typography.subtitle2)
+            }
+            Text(text = "+${completedData.bonus} exp", style = MaterialTheme.typography.subtitle2)
+        }
+    }
+}
---
+++ app/src/main/java/com/syafi/ecoquest/presentation/profil/component/Experience.kt
@@ -0,0 +1,2 @@
+package com.syafi.ecoquest.presentation.profil.component
+
